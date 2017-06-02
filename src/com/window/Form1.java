2c886e9a04469e484da394940279621956019356
Serob Balyan <serob.balyan@gmail.com>
2017-06-03 00:59:50 +0300

    Some Java 7 and Java 8 features

--- src/com/window/Form1.java
+++ src/com/window/Form1.java
@@ -1,13 +1,6 @@
 package com.window;
 
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JFrame;
-import javax.swing.JTabbedPane;
+import javax.swing.*;
 import javax.swing.border.Border;
 
 import com.exceptions.ContainerSizeException;
@@ -28,13 +21,15 @@ import java.io.File;
 public class Form1 extends JPanel {
 
 	private static final long serialVersionUID = 1L;
+
+	private static final String FULL = "FULL_";
 	
 	private JFileChooser fcContainer;
 	private JFileChooser fcSecret;
 	
-	private Label containerLable;
-	private Label secretLable;
-	private Label fullContainerLable;
+	private Label containerLabel;
+	private Label secretLabel;
+	private Label fullContainerLabel;
 	
 	private File container;
 	private File fullContainer;
@@ -62,14 +57,14 @@ public class Form1 extends JPanel {
 		JButton b1 = new JButton("Choose container file ...", createImageIcon("/com/resources/img/Open16.gif"));
 		b1.setBounds(35, 70, 180, 30);
 		b1.addActionListener(new ContainerOpenListener());
-		containerLable = new Label();
-		containerLable.setBounds(40, 100, 170, 30);
+		containerLabel = new Label();
+		containerLabel.setBounds(40, 100, 170, 30);
 		
 		JButton b2 = new JButton("Choose secret file ...", createImageIcon("/com/resources/img/Open16.gif"));
 		b2.setBounds(280, 70, 180, 30);
 		b2.addActionListener(new SecretOpenListener());
-		secretLable = new Label();
-		secretLable.setBounds(285, 100, 170, 30);
+		secretLabel = new Label();
+		secretLabel.setBounds(285, 100, 170, 30);
 		
 		JButton b3 = new JButton("Hide data", createImageIcon("/com/resources/img/Save16.gif"));
 		b3.setBounds(190, 230, 120, 50);
@@ -78,8 +73,8 @@ public class Form1 extends JPanel {
 		hidePane.add(b1);
 		hidePane.add(b2);
 		hidePane.add(b3);
-		hidePane.add(containerLable);
-		hidePane.add(secretLable);
+		hidePane.add(containerLabel);
+		hidePane.add(secretLabel);
 		
 		JPanel extractPane = new JPanel();
 		extractPane.setBorder(paneEdge);
@@ -88,8 +83,8 @@ public class Form1 extends JPanel {
 		JButton b4 = new JButton("Choose container file ...", createImageIcon("/com/resources/img/Open16.gif"));
 		b4.setBounds(160, 70, 180, 30);
 		b4.addActionListener(new FullContainerOpenListener());
-		fullContainerLable = new Label();
-		fullContainerLable.setBounds(165, 100, 170, 30);
+		fullContainerLabel = new Label();
+		fullContainerLabel.setBounds(165, 100, 170, 30);
 		
 		JButton b5 = new JButton("Extract data", createImageIcon("/com/resources/img/Save16.gif"));
 		b5.setBounds(180, 230, 140, 50);
@@ -98,7 +93,7 @@ public class Form1 extends JPanel {
 		
 		extractPane.add(b4);
 		extractPane.add(b5);
-		extractPane.add(fullContainerLable);
+		extractPane.add(fullContainerLabel);
 
 		JTabbedPane tabbedPane = new JTabbedPane();
 		tabbedPane.addTab("Hiding", null, hidePane, null);
@@ -120,14 +115,14 @@ public class Form1 extends JPanel {
 	private class ContainerOpenListener implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			container = containerAction(containerLable);
+			container = containerAction(containerLabel);
 		}
 	}
 	
 	private class FullContainerOpenListener implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			fullContainer = containerAction(fullContainerLable);
+			fullContainer = containerAction(fullContainerLabel);
 		}
 	}
 	
@@ -138,7 +133,7 @@ public class Form1 extends JPanel {
 			int returnVal = fcSecret.showOpenDialog(Form1.this);
 			if (returnVal == JFileChooser.APPROVE_OPTION) {
 				secret = fcSecret.getSelectedFile();
-                secretLable.setText(secret.getName());
+                secretLabel.setText(secret.getName());
 	        } 
 		}
 	}
@@ -148,16 +143,16 @@ public class Form1 extends JPanel {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			if (container == null || secret == null){
-				JOptionPane.showMessageDialog(Form1.this, "You must choose container AND secret files!", "ERROR!", JOptionPane.OK_OPTION);
+				JOptionPane.showMessageDialog(Form1.this, "You must choose container AND secret files!", "ERROR!", JOptionPane.ERROR_MESSAGE);
 			} else {
 				try {
 					byte[] fullContainerBytes = Stego.hide(container, secret);
-					String newName = FileUtils.getFilePathOnly(container) + "\\FULL_"  + container.getName();
+					String newName = container.getParent() + File.separator + FULL  + container.getName();
 					FileUtils.byteArrayToFile(newName, fullContainerBytes);
-					JOptionPane.showMessageDialog(Form1.this, "Full container now is \'FULL_"  + container.getName() + "\'");
+					JOptionPane.showMessageDialog(Form1.this, "Full container now is \'" + FULL + container.getName() + "\'");
 				} catch (ContainerSizeException e1) {
 					e1.printStackTrace();
-					JOptionPane.showMessageDialog(Form1.this, e1.getMessage(),  "ERROR!", JOptionPane.OK_OPTION);
+					JOptionPane.showMessageDialog(Form1.this, e1.getMessage(),  "ERROR!", JOptionPane.WARNING_MESSAGE);
 				}
 			}
 		}
@@ -169,18 +164,16 @@ public class Form1 extends JPanel {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			if(fullContainer == null){
-				JOptionPane.showMessageDialog(Form1.this, "You must choose full container!", "ERROR!", JOptionPane.OK_OPTION);
+				JOptionPane.showMessageDialog(Form1.this, "You must choose full container!", "ERROR!", JOptionPane.ERROR_MESSAGE);
 			} else {
 				try{
 					StringBuilder secretsName = new StringBuilder();
-					String newPath;
 					byte[] secretBytes =Stego.extract(fullContainer, secretsName);
-					newPath = FileUtils.getFilePathOnly(fullContainer) + "\\" + secretsName.toString();
+					String newPath = fullContainer.getParent() + File.separator + secretsName.toString();
 					FileUtils.byteArrayToFile(newPath, secretBytes);
 					JOptionPane.showMessageDialog(Form1.this, "The extracted file is \'"  + secretsName.toString() + "\'");
-				
 				} catch (OutOfMemoryError e1){
-					JOptionPane.showMessageDialog(Form1.this, "Choose the correct container!", "ERROR!", JOptionPane.OK_OPTION);
+					JOptionPane.showMessageDialog(Form1.this, "Choose the correct container!", "ERROR!", JOptionPane.ERROR_MESSAGE);
 				}
 			}
 			
@@ -190,7 +183,7 @@ public class Form1 extends JPanel {
 	}
 	
 	 /** Returns an ImageIcon, or null if the path was invalid. */
-    protected static ImageIcon createImageIcon(String path) {
+    private static ImageIcon createImageIcon(String path) {
         java.net.URL imgURL = Form1.class.getResource(path);
         if (imgURL != null) {
             return new ImageIcon(imgURL);
@@ -207,8 +200,7 @@ public class Form1 extends JPanel {
      */
     private static void createAndShowGUI() {
     	JFrame frame = new JFrame("Hiding");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         
         Form1 form1 = new Form1();
         //newContentPane.setLayout(null);
@@ -225,11 +217,7 @@ public class Form1 extends JPanel {
 	public static void main(String[] args) {
 	        //Schedule a job for the event-dispatching thread:
 	        //creating and showing this application's GUI.
-	        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-	            public void run() {
-	                createAndShowGUI();
-	            }
-	        });
+	        javax.swing.SwingUtilities.invokeLater(Form1::createAndShowGUI);
 	    }
 
 }
